a36c7fb5798f31e116f440c0264310c4c56b035c
kisekitw <kisekitw@gmail.com>
2018-08-03 07:46:39 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('Starting app.');
+
+const fs = require('fs');
+const os = require('os');
+
+var user =  os.userInfo();
+
+console.log(user);
+
+
+fs.appendFileSync('greetings.txt', `Hello ${user.username}!!!` );
