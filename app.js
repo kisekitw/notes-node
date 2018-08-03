bf9a00c6f7d1348990179bab0d7c7dafa6ae76a7
kisekitw <kisekitw@gmail.com>
2018-08-03 08:25:47 +0800

    Using 3rd party modules

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-console.log(notes.add(10, -2));
+// console.log(notes.add(10, -2));
 
 // var user =  os.userInfo();
 // fs.appendFileSync('greetings.txt', `Hello ${user.username}!!!` );
