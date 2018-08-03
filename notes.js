d4eb71cc6a3b3ba3dcd54b426df0aae87c7630a6
kisekitw <kisekitw@gmail.com>
2018-08-03 23:23:53 +0800

    Adding and Saving Notes

--- notes.js
+++ notes.js
@@ -1,11 +1,31 @@
 console.log('Starting notes.js')
 
+const fs = require('fs');
+
 // module.exports.addNote = (title, body) => {
 //     console.log('addNote');
 //     return 'New Note';
 // };
+
 var addNote = (title, body) => {
-    console.log('Adding Note', title, body);
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (error) {
+        
+    }
+
+    var duplicateNotes = notes.filter((note) => note.title === title);
+    if(duplicateNotes.length === 0){
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
 };
 
 var getAll = () => {
