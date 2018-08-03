4876d7c820718ea333d36d0d8db20945bdf0aa83
kisekitw <kisekitw@gmail.com>
2018-08-03 18:38:21 +0800

    Working with Json

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ const notes = require('./notes.js');
 const argv  = yargs.argv;
 var command = argv._[0];
 
-// console.log('Command : ' + command);
-// console.log("Process : " + process.argv);
-// console.log("Yargs : " + yargs.title);
-
 if(command === 'add') {
     notes.addNote(argv.title, argv.body);
 }else if(command === 'list') {
